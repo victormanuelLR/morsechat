eb7a6d2f50adbfd08a89db49139d964354e3ffce
Victor Manuel LR <vleal3423@gmail.com>
2025-02-04 12:07:36 -0300

    static test

--- morsechat/asgi.py
+++ morsechat/asgi.py
@@ -1,6 +1,7 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
