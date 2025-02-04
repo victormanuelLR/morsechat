fb47c7b6f48b4d271a7f386d52eb1c33da332219
Victor Manuel LR <vleal3423@gmail.com>
2025-02-04 20:36:29 -0300

    site design finished

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.principalpage, name="principalpage"),
     path('signup/', views.signup, name="signup"),
     path("login/", auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- room/urls.py
+++ room/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.rooms, name='room'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('<slug:slug>/', views.room, name='room'),
 ]
