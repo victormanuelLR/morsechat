071204ace3272e7505efebe908f554f0b62a7e31
Victor Manuel LR <vleal3423@gmail.com>
2025-02-04 20:43:51 -0300

    image test

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ set -o errexit
 pip install -r requirements.txt 
 
 python manage.py collectstatic --no-input 
+python manage.py findstatic core/images/morse-chat.png
+
 python manage.py migrate 
 if [[ $CREATE_SUPERUSER ]]; 
 then 
