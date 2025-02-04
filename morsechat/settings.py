4ea4e352df7ae9d106fc9e809775c39108f6743d
Victor Manuel LR <vleal3423@gmail.com>
2025-02-04 11:17:13 -0300

    add staticfiles_sotarage

--- morsechat/settings.py
+++ morsechat/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
