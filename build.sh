9c34142330a8f62048c809409ff808e7648d1b56
Victor Manuel LR <vleal3423@gmail.com>
2025-02-04 21:06:53 -0300

    add new process to build.sh 'python manage.py shell ...'

--- build.sh
+++ build.sh
@@ -12,4 +12,6 @@ python manage.py migrate
 if [[ $CREATE_SUPERUSER ]]; 
 then 
 python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL" 
+python manage.py shell -c "from room.models import Room; Room.objects.get_or_create(name='Morsechat', slug='default-room')"
+
 fi
