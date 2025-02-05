5b75fc15b4728891f091e0e4811a89cc32fd16e1
Victor Manuel LR <vleal3423@gmail.com>
2025-02-04 22:49:15 -0300

    user can now create a room

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,12 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2',]
+
+class CreateRoom(forms.Form):
+    roomname = forms.CharField(max_length=100)
--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from .forms import SignUpForm, CreateRoom
+from room.models import Room
 # Create your views here.
 
 def principalpage(request):
-    return render(request, 'core/principalpage.html')
+    if request.method == "POST":
+        form = CreateRoom(request.POST)
+        if form.is_valid():
+            roomname = form.cleaned_data["roomname"]
+
+            Room.objects.get_or_create(name=roomname, slug=roomname.lower().replace(" ", "-"))
+
+            return redirect("room")
+
+    else:
+        return render(request, 'core/principalpage.html')
 
 
 def signup(request):
